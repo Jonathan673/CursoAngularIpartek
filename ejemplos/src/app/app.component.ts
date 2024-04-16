8d4e895518a8da870c8cd3101fcdf252206139cf
Jonathan673 <jonathan.camac@gmail.com>
2024-04-16 20:28:31 +0200

    Titulo personalizable por variable

--- ejemplos/src/app/app.component.ts
+++ ejemplos/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TituloComponent } from './titulo.component';
+import { NavBarComponent } from './navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TituloComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ ejemplos/src/app/titulo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'titulo',
+    template: '<h1>{{texto}}</h1>',
+    standalone: true,
+})
+export class TituloComponent{
+    texto = 'Título personalizado';
+}
