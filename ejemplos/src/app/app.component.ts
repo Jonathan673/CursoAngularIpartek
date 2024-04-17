a62c31ee436c1c2134c42faa3e918658755800af
Jonathan673 <jonathan.camac@gmail.com>
2024-04-17 19:04:21 +0200

    componente incluido en el app-route

--- ejemplos/src/app/app.component.ts
+++ ejemplos/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TituloComponent } from './titulo.component';
-import { NavBarComponent } from './navbar.component';
 import { LabelInputComponent } from './labelinput.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TituloComponent, NavBarComponent, LabelInputComponent],
+  imports: [RouterOutlet, TituloComponent, LabelInputComponent, CabeceraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
