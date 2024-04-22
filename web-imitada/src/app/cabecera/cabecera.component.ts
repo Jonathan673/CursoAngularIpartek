d4135aaa028c7a9fea0127e2439cc9aa637c829b
Jonathan673 <jonathan.camac@gmail.com>
2024-04-22 10:20:15 +0200

    vista de todos los componentes en pantalla

--- web-imitada/src/app/cabecera/cabecera.component.ts
+++ web-imitada/src/app/cabecera/cabecera.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MenudesplegableComponent } from '../menudesplegable/menudesplegable.component';
+import { MenuprincipalComponent } from '../menuprincipal/menuprincipal.component';
+import { BarrabusquedaComponent } from '../barrabusqueda/barrabusqueda.component';
+import { RedessocialesComponent } from '../redessociales/redessociales.component';
 
 @Component({
   selector: 'app-cabecera',
   standalone: true,
-  imports: [],
+  imports: [MenudesplegableComponent,MenuprincipalComponent,BarrabusquedaComponent,RedessocialesComponent],
   templateUrl: './cabecera.component.html',
   styleUrl: './cabecera.component.css'
 })
