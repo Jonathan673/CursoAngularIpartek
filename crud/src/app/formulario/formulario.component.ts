dc1efddc68bcb325a32dd7cb72265bdcf89f2aa0
Jonathan673 <jonathan.camac@gmail.com>
2024-04-24 21:02:59 +0200

    editar muestra los datos en el formulario

--- crud/src/app/formulario/formulario.component.ts
+++ crud/src/app/formulario/formulario.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProductoService } from '../producto.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
@@ -13,11 +14,21 @@ export class FormularioComponent {
   ruta = inject(ActivatedRoute);
   servicio = inject(ProductoService);
 
+
+  form = new FormGroup({
+    id: new FormControl(0),
+    nombre: new FormControl(''),
+    precio: new FormControl(0),
+  })
+
+
+
   constructor (){
     const id = Number(this.ruta.snapshot.params['id']);
     if(id){
       this.servicio.obtenerPorId(id).then(productoRecibido => {
-        console.log(productoRecibido);
+        //coge todos los valores del producto recibido y metelos en el formulario
+        this.form.setValue(productoRecibido);
       });
     }
   }
