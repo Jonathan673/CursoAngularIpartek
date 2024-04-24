8b4cf934774d6bfa6c6cc3f9ff6a9835cf8ba8ec
Jonathan673 <jonathan.camac@gmail.com>
2024-04-24 19:44:48 +0200

    Configuramos las rutas, modo fácil

--- crud/src/app/app.routes.ts
+++ crud/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListadoComponent } from './listado/listado.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    //pATH vacio es mi página inicial
+    {path:'', component:ListadoComponent},
+    {path:'formulario', component:FormularioComponent},
+];
