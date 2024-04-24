cc611f09bcd39b73fed290f7d8279367eab0c68b
Jonathan673 <jonathan.camac@gmail.com>
2024-04-24 20:11:46 +0200

    listado con modelo de productos, sin json

--- crud/src/app/listado/listado.component.ts
+++ crud/src/app/listado/listado.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Producto } from '../producto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listado',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './listado.component.html',
   styleUrl: './listado.component.css'
 })
 export class ListadoComponent {
-
+  productos: Producto[] = [
+    {id:1,nombre:'Monitor',precio:31.32},
+    {id:2,nombre:'Monitor2',precio:32.32},
+    {id:3,nombre:'Monitor3',precio:33.32},
+  ] 
 }
