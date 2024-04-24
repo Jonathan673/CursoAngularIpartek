87a2fff9863256f8d9a79291ccbb6f7f71e0ca47
Jonathan673 <jonathan.camac@gmail.com>
2024-04-24 20:24:55 +0200

    Conexión del listado con el servicio

--- crud/src/app/listado/listado.component.ts
+++ crud/src/app/listado/listado.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Producto } from '../producto';
 import { CommonModule } from '@angular/common';
+import { ProductoService } from '../producto.service';
 
 @Component({
   selector: 'app-listado',
   standalone: true,
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './listado.component.html',
   styleUrl: './listado.component.css'
 })
 export class ListadoComponent {
-  productos: Producto[] = [
-    {id:1,nombre:'Monitor',precio:31.32},
-    {id:2,nombre:'Monitor2',precio:32.32},
-    {id:3,nombre:'Monitor3',precio:33.32},
-  ] 
+  servicio: ProductoService = inject(ProductoService);
+  productos: Producto[] = [];
+
+  constructor() {
+    this.productos = this.servicio.obtenerTodos();
+  }
 }
--- crud/src/app/producto.service.ts
+++ crud/src/app/producto.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Producto } from './producto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
+  productos: Producto[] = [
+    { id: 1, nombre: 'Monitor', precio: 31.32 },
+    { id: 2, nombre: 'Monitor2', precio: 32.32 },
+    { id: 3, nombre: 'Monitor3', precio: 33.32 },
+  ]
 
-  constructor() { }
+  obtenerTodos(): Producto[] {
+    return this.productos
+  }
 }
