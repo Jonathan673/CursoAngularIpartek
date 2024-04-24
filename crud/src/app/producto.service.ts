68f29a8d3c4d6a5f9742cf0119db40248531f7b9
Jonathan673 <jonathan.camac@gmail.com>
2024-04-24 20:28:58 +0200

    base de datos para json server

--- crud/src/app/producto.service.ts
+++ crud/src/app/producto.service.ts
@@ -5,11 +5,7 @@ import { Producto } from './producto';
   providedIn: 'root'
 })
 export class ProductoService {
-  productos: Producto[] = [
-    { id: 1, nombre: 'Monitor', precio: 31.32 },
-    { id: 2, nombre: 'Monitor2', precio: 32.32 },
-    { id: 3, nombre: 'Monitor3', precio: 33.32 },
-  ]
+
 
   obtenerTodos(): Producto[] {
     return this.productos
