234d6d9bba8ac97477505eb84edcfd6920a11c0f
Jonathan673 <jonathan.camac@gmail.com>
2024-04-30 19:27:42 +0200

    Handler error de angular, yo prefiero la versión anterior pero ya que son parte de los tutoriales de angular los voy a usar

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -18,24 +18,39 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
       tap(_ => this.log('Se han pedido los heroes')),
-      catchError(_ => {
-        this.log('Error al recibir los heroes');
-        return of([])})
-    )
+      catchError(this.handleError<Hero[]>('hetHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
     return this.http.get<Hero>(`${this.heroesUrl}/${id}`).pipe(
       tap(_ => this.log('Se han pedido los heroes')),
-      catchError(_ => {
-        this.log('Error al pedir el heroe');
-        return of({} as Hero);
-      })
-    )
+      catchError(this.handleError<Hero>('getHero'))
+    );
   }
 
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ *
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
