0bd4597f74eb53ee91ba3c996c20edcaff1458ce
Jonathan673 <jonathan.camac@gmail.com>
2024-05-02 19:29:33 +0200

    Lipieza de una variable que no se esta utilizando

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,7 +9,6 @@ import { MessageService } from '../message.service';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
